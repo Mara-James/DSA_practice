d9de936fbdf98eafe551f8393c1469b148b20162
Mara Cunningham <mara.james.codes@gmail.com>
2023-10-16 16:05:43 -0500

    creating test cases

--- Grokking_DSA_Patterns/Warm_ups.py
+++ Grokking_DSA_Patterns/Warm_ups.py
@@ -2,5 +2,17 @@
 """ This file contains warm up exercises for Grocking the Coding interview"""
 
 
-###      CREATES DUPLICATE (EASY)     ###
+###      CONTAINS DUPLICATE (EASY)     ###
+
+
+#  - Test Cases - #
+test1 = [1,2,3,4]
+test2 = [1,2,3,1]
+
+class Solution:
+    def containsDuplicate(self, nums):
+      target_num = None
+
+      for index,num in nums:
+        
 
